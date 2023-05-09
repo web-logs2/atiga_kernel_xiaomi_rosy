bc94b901f8b6c11e6fb41fffd655b6ca397e3aa0
Aditya Eka Pranata <aepranata@gmail.com>
2023-05-09 01:33:09 +0000

    Add 'drivers/kernelsu/' from commit '33ea9a6f3303b123a860d9fa7bc702a5927490f9'
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1673,6 +1673,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
